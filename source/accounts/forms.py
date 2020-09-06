549dee30b19d14c1376aa04a1a787add1e51aabb
Azat Kaparov <azatkaparov843@gmail.com>
2020-09-06 14:08:59 +0600

    Добавлена регистрация

---
+++ source/accounts/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django import forms
+
+
+class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField(label='Email', required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2',
+                  'first_name', 'last_name', 'email']
+        field_classes = {'username': UsernameField}
